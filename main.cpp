46a0bf48f1ef1e10b640482df310c28c0faa4819
luisantonioselisvasquez-hub <luisantonioselisvasquez@gmail.com>
2026-03-30 21:48:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -85,6 +85,9 @@ int main() {
     // 20 Segura Nuñez Alex Ernesto <alex.segura.nunez@gmail.com>
 
     // 21 Selis Vasquez Luis Antonio <luisanonioselisvasquez@gmail.com>
-
+    T1 a1 = 5, b1 = 6; 
+    T1 r0 = (a1*a1 + b1*b1)/(a1 + b1); 
+    cout << r0 << endl;
+    
     return 0;
 }
